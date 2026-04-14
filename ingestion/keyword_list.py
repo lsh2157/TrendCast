50e40106581db3f7cefd58cecccd3df9e99cd326
lsh2157 <lsh2157@columbia.edu>
2026-04-13 20:50:27 -0400

    Create keyword_list.py

---
+++ ingestion/keyword_list.py
@@ -0,0 +1,30 @@
+KEYWORDS = {
+    "categorical": [
+        "smartphone",
+        "laptop",
+        "tablet",
+        "smartwatch",
+        "earbuds",
+        "monitor",
+        "smart TV"
+    ],
+    "company": [
+        "Apple",
+        "NVIDIA",
+        "Samsung",
+        "Sony",
+        "Microsoft",
+        "Intel",
+        "AMD",
+        "Logitech"
+    ],
+    "feature_focused": [
+        "noise cancelling headphones",
+        "OLED monitor",
+        "mechanical keyboard",
+        "wireless charger",
+        "4K webcam",
+        "bluetooth speaker",
+        "streaming stick"
+    ]
+}
